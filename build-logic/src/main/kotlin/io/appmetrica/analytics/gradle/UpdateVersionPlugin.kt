0c1b4bbc93f15e86be0e8c87b8c2d4fbad8c90c7
alex98 <alex98@yandex-team.com>
2023-07-28 11:37:55 +0300

    up AppMetrica SDK Android to 6.0.0-RC

--- build-logic/src/main/kotlin/io/appmetrica/analytics/gradle/UpdateVersionPlugin.kt
+++ build-logic/src/main/kotlin/io/appmetrica/analytics/gradle/UpdateVersionPlugin.kt
@@ -4,7 +4,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import java.io.File
 
-private val VERSION_REG = Regex("[0-9]+\\.[0-9]+\\.[0-9]+")
+private val VERSION_REG = Regex("[0-9]+\\.[0-9]+\\.[0-9]+(-RC)?")
 
 class UpdateVersionPlugin : Plugin<Project> {
 
@@ -48,14 +48,15 @@ class UpdateVersionPlugin : Plugin<Project> {
 
     private fun Project.updateConstants(version: String) {
         val filePath = "build-logic/src/main/kotlin/io/appmetrica/analytics/gradle/Constants.kt"
-        updateVersion(filePath, ".*object Library.*\n.*const val versionName = \"", "\"", version)
+        updateVersion(filePath, ".*object Library.*\\n.*const val versionName = \"", "\"", version)
     }
 
     private fun Project.updateGradleConstants(version: String) {
-        val filePath = "../appmetrica/appmetrica-sample/constants.gradle"
-        val versionCode = getVersionCodeForSample(version).also(::println)
-        updateVersion(filePath, ".*sampleVersion = '", "'", version)
-        updateVersionCode(filePath, ".*sampleVersionCodePrefix = '", "'", versionCode)
+        val filePath = "../appmetrica-sample/constants.gradle"
+        val versionCode = getVersionCodeForSample(version)
+        updateVersion(filePath, ".*sampleVersion = \"", "\"", version)
+        updateVersion(filePath, ".*appmetricaSnapshotVersion = \"", "\"", version)
+        updateVersionCode(filePath, ".*sampleVersionCodePrefix = \"", "\"", versionCode)
     }
 
     private fun Project.updateSdkDataClass(version: String) {
@@ -72,33 +73,35 @@ class UpdateVersionPlugin : Plugin<Project> {
     private fun Project.updateVersionInAutotests(version: String) {
         updateVersionInAutotestsVersionProvider(
             version,
-            "../../autotests/appmetrica-sdk/framework/appmetrica.build/src/main/java/com/yandex/appmetrica/autotests/android/NativeAndroidMainVersionsProvider.kt"
+            "../../../autotests/appmetrica-sdk/framework/appmetrica.build/src/main/java/com/yandex/appmetrica/autotests/android/NativeAndroidMainVersionsProvider.kt"
         )
         updateVersionInAutotestsVersionProvider(
             version,
-            "../../autotests/appmetrica-sdk/framework/appmetrica.build/src/main/java/com/yandex/appmetrica/autotests/android/NativeAndroidYandexVersionsProvider.kt"
+            "../../../autotests/appmetrica-sdk/framework/appmetrica.build/src/main/java/com/yandex/appmetrica/autotests/android/NativeAndroidYandexVersionsProvider.kt"
         )
     }
 
     private fun getVersionCodeForAutotests(version: String): String {
-        val items = version.split(".")
+        val items = version.substringBefore("-").split(".")
         return (listOf(items[0]) + items.subList(1, 3).map { it.padStart(3, '0') }).joinToString("_")
     }
 
     private fun getVersionCodeForSample(version: String): String {
-        val items = version.split(".")
+        val items = version.substringBefore("-").split(".")
         return (listOf(items[0]) + items.subList(1, 3).map { it.padStart(3, '0') }).joinToString("")
     }
 
     private fun Project.updateFile(filePath: String, beforeReg: String, replaceReg: String, afterReg: String, newText: String) {
         val file = file(filePath)
-        check(file.exists()) { "Not found file $filePath" }
+        check(file.exists()) { "Not found file ${file.canonicalPath}" }
 
-        val regex = Regex("($beforeReg)$replaceReg($afterReg)")
+        val regex = Regex(
+            "($beforeReg)$replaceReg($afterReg)",
+            setOf(RegexOption.MULTILINE)
+        )
         val text = file.readText()
-        val matcher = regex.matchEntire(file.readText())
-        checkNotNull(matcher == null) { "Not found regex $regex in file $filePath" }
-        file.writeText(text.replace(regex, "\$1$newText\$2"))
+        check(regex.findAll(text).toList().isNotEmpty()) { "Not found regex $regex in file $filePath" }
+        file.writeText(text.replace(regex) { "${it.groupValues[1]}$newText${it.groupValues.last()}" })
     }
 
     private fun Project.updateVersion(filePath: String, beforeReg: String, afterReg: String, version: String) {
