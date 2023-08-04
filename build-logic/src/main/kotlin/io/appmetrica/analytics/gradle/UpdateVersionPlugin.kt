0e5469703ef46b20e6e3d5352cbc1cf32babb608
alex98 <alex98@yandex-team.com>
2023-08-04 11:06:55 +0300

    move updateVersion task

--- build-logic/src/main/kotlin/io/appmetrica/analytics/gradle/UpdateVersionPlugin.kt
+++ build-logic/src/main/kotlin/io/appmetrica/analytics/gradle/UpdateVersionPlugin.kt
@@ -4,26 +4,9 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import java.io.File
 
-private val VERSION_REG = Regex("[0-9]+\\.[0-9]+\\.[0-9]+(-RC[0-9]*)?")
-
 class UpdateVersionPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        project.tasks.register("updateVersion") {
-            doLast {
-                val version = project.properties["version"].toString()
-                require(version.matches(VERSION_REG)) {
-                    "Failed version format: '$version'. Version should be in format '${VERSION_REG}'. " +
-                        "Use ./gradlew updateVersion -Pversion=NEW_VERSION"
-                }
-
-                project.updateConstants(version)
-                project.updateGradleConstants(version)
-                project.updateSdkDataClass(version)
-                project.updateVersionInCi(version)
-                project.updateVersionInAutotests(version)
-            }
-        }
         registerUpdateRobolectricTask(project)
     }
 
@@ -45,76 +28,5 @@ class UpdateVersionPlugin : Plugin<Project> {
                 }
             }
         }
-    }
-
-    private fun Project.updateConstants(version: String) {
-        val filePath = "build-logic/src/main/kotlin/io/appmetrica/analytics/gradle/Constants.kt"
-        updateVersion(filePath, ".*object Library.*\\n.*const val versionName = \"", "\"", version)
-    }
-
-    private fun Project.updateGradleConstants(version: String) {
-        val filePath = "../appmetrica-sample/constants.gradle"
-        val versionCode = getVersionCodeForSample(version)
-        updateVersion(filePath, ".*sampleVersion = \"", "\"", version)
-        updateVersion(filePath, ".*appmetricaSnapshotVersion = \"", "\"", version)
-        updateVersionCode(filePath, ".*sampleVersionCodePrefix = \"", "\"", versionCode)
-    }
-
-    private fun Project.updateSdkDataClass(version: String) {
-        val filePath = "analytics/src/main/java/io/appmetrica/analytics/impl/SdkData.java"
-        updateVersion(filePath, ".*CURRENT_VERSION_NAME_FOR_MAPPING = \"", "\";", version)
-    }
-
-    private fun Project.updateVersionInCi(version: String) {
-        val filePath = "../a.yaml"
-        updateVersion(filePath, ".*appmetrica-sdk/r-", "-.*", version)
-    }
-
-    private fun Project.updateVersionInAutotestsVersionProvider(version: String, filePath: String) {
-        val versionCode = getVersionCodeForAutotests(version)
-        updateVersion(filePath, ".*getDevVersionName\\(\"", "\"\\)", version)
-        updateVersionCode(filePath, ".*devVersion\\(", ", \"dev\".*", versionCode)
-    }
-
-    private fun Project.updateVersionInAutotests(version: String) {
-        updateVersionInAutotestsVersionProvider(
-            version,
-            "../../../autotests/appmetrica-sdk/framework/appmetrica.build/src/main/java/com/yandex/appmetrica/autotests/android/NativeAndroidMainVersionsProvider.kt"
-        )
-        updateVersionInAutotestsVersionProvider(
-            version,
-            "../../../autotests/appmetrica-sdk/framework/appmetrica.build/src/main/java/com/yandex/appmetrica/autotests/android/NativeAndroidYandexVersionsProvider.kt"
-        )
-    }
-
-    private fun getVersionCodeForAutotests(version: String): String {
-        val items = version.substringBefore("-").split(".")
-        return (listOf(items[0]) + items.subList(1, 3).map { it.padStart(3, '0') }).joinToString("_")
-    }
-
-    private fun getVersionCodeForSample(version: String): String {
-        val items = version.substringBefore("-").split(".")
-        return (listOf(items[0]) + items.subList(1, 3).map { it.padStart(3, '0') }).joinToString("")
-    }
-
-    private fun Project.updateFile(filePath: String, beforeReg: String, replaceReg: String, afterReg: String, newText: String) {
-        val file = file(filePath)
-        check(file.exists()) { "Not found file ${file.canonicalPath}" }
-
-        val regex = Regex(
-            "($beforeReg)$replaceReg($afterReg)",
-            setOf(RegexOption.MULTILINE)
-        )
-        val text = file.readText()
-        check(regex.findAll(text).toList().isNotEmpty()) { "Not found regex $regex in file $filePath" }
-        file.writeText(text.replace(regex) { "${it.groupValues[1]}$newText${it.groupValues.last()}" })
-    }
-
-    private fun Project.updateVersion(filePath: String, beforeReg: String, afterReg: String, version: String) {
-        updateFile(filePath, beforeReg, VERSION_REG.toString(), afterReg, version)
-    }
-
-    private fun Project.updateVersionCode(filePath: String, beforeReg: String, afterReg: String, version: String) {
-        updateFile(filePath, beforeReg, "[0-9_]+", afterReg, version)
     }
 }
--- update-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-set -e
-
-./gradlew updateVersion -Pversion="$1"
-./aarDump.sh
