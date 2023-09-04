454b7a8de681ecfb86ac86e3d8c6883cd872801e
robot-frankenstein <robot-frankenstein@yandex-team.com>
2023-09-04 10:46:16 +0300

    up AppMetrica SDK Android version to 6.0.0

--- build-logic/src/main/kotlin/io/appmetrica/analytics/gradle/Constants.kt
+++ build-logic/src/main/kotlin/io/appmetrica/analytics/gradle/Constants.kt
@@ -16,7 +16,7 @@ object Constants {
     }
 
     object Library {
-        const val versionName = "6.0.0-RC1"
+        const val versionName = "6.0.0"
         val versionCode = BUILD_NUMBER.toInt()
         val buildNumber = BUILD_NUMBER
         //Also need to update api level at https://nda.ya.ru/t/rFFwGoT66NsxJm and https://nda.ya.ru/t/4_qxHC-N6NsxL4
