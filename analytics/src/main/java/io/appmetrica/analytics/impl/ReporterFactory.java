954500582cd526144994093e037efa3ea8d87a0c
alexklints <alexklints@yandex-team.com>
2024-05-24 16:47:22 +0300

    fix probably NPE at io.appmetrica.analytics.impl.Qb.b(SourceFile:10) e8ec2da1d0621442c35e05233ada9f37632f1894

--- analytics/src/main/java/io/appmetrica/analytics/impl/ReporterFactory.java
+++ analytics/src/main/java/io/appmetrica/analytics/impl/ReporterFactory.java
@@ -3,6 +3,12 @@ package io.appmetrica.analytics.impl;
 import android.content.Context;
 import android.os.Handler;
 import android.util.Log;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.WorkerThread;
 import io.appmetrica.analytics.AppMetricaConfig;
@@ -15,28 +21,31 @@ import io.appmetrica.analytics.impl.utils.validation.ThrowIfFailedValidator;
 import io.appmetrica.analytics.impl.utils.validation.Validator;
 import io.appmetrica.analytics.impl.utils.validation.api.ReporterKeyIsUsedValidator;
 import io.appmetrica.analytics.logger.internal.DebugLogger;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class ReporterFactory implements IReporterFactory {
 
     private static final String TAG = "[ReporterFactory]";
 
-    @NonNull private Context mContext;
-    @NonNull private ProcessConfiguration mProcessConfiguration;
-    @NonNull private ReportsHandler mReportsHandler;
-    @NonNull private Handler mTasksHandler;
-    @NonNull private StartupHelper mStartupHelper;
-
-    private Map<String, IReporterExtended> mReporters = new HashMap<String, IReporterExtended>();
-    private final Validator<String> mReporterApiKeyUsedValidator = new ThrowIfFailedValidator<String>(
-            new ReporterKeyIsUsedValidator(mReporters)
+    @NonNull
+    private final Context context;
+    @NonNull
+    private final ProcessConfiguration processConfiguration;
+    @NonNull
+    private final ReportsHandler reportsHandler;
+    @NonNull
+    private final Handler tasksHandler;
+    @NonNull
+    private final StartupHelper startupHelper;
+    @NonNull
+    private final Map<String, IReporterExtended> reporters = new HashMap<>();
+    @NonNull
+    private final Validator<String> reporterApiKeyUsedValidator = new ThrowIfFailedValidator<>(
+        new ReporterKeyIsUsedValidator(reporters)
     );
-    private final List<String> mApiKeysToIgnoreStartup = Arrays.asList(
-            SdkData.SDK_API_KEY_UUID,
-            SdkData.SDK_API_KEY_PUSH_SDK
+    @NonNull
+    private final List<String> apiKeysToIgnoreStartup = Arrays.asList(
+        SdkData.SDK_API_KEY_UUID,
+        SdkData.SDK_API_KEY_PUSH_SDK
     );
 
     public ReporterFactory(@NonNull Context context,
@@ -44,75 +53,84 @@ public class ReporterFactory implements IReporterFactory {
                            @NonNull ReportsHandler reportsHandler,
                            @NonNull Handler tasksHandler,
                            @NonNull StartupHelper startupHelper) {
-        mContext = context;
-        mProcessConfiguration = processConfiguration;
-        mReportsHandler = reportsHandler;
-        mTasksHandler = tasksHandler;
-        mStartupHelper = startupHelper;
+        this.context = context;
+        this.processConfiguration = processConfiguration;
+        this.reportsHandler = reportsHandler;
+        this.tasksHandler = tasksHandler;
+        this.startupHelper = startupHelper;
     }
 
     @NonNull
     @WorkerThread
     @Override
-    public MainReporter buildMainReporter(@NonNull AppMetricaConfig config,
-                                          final boolean needToClearEnvironment) {
-        mReporterApiKeyUsedValidator.validate(config.apiKey);
+    public MainReporter buildMainReporter(
+        @NonNull AppMetricaConfig config,
+        final boolean needToClearEnvironment
+    ) {
+        reporterApiKeyUsedValidator.validate(config.apiKey);
 
         final MainReporter mainReporter = new MainReporter(
-                mContext,
-                mProcessConfiguration,
-                config, mReportsHandler,
-                mStartupHelper,
-                new UnhandledSituationReporterProvider(this, SdkData.SDK_API_KEY_UUID),
-                new UnhandledSituationReporterProvider(this, SdkData.SDK_API_KEY_PUSH_SDK)
+            context,
+            processConfiguration,
+            config, reportsHandler,
+            startupHelper,
+            new UnhandledSituationReporterProvider(this, SdkData.SDK_API_KEY_UUID),
+            new UnhandledSituationReporterProvider(this, SdkData.SDK_API_KEY_PUSH_SDK)
         );
 
         performCommonInitialization(mainReporter);
         mainReporter.updateConfig(config, needToClearEnvironment);
         mainReporter.start();
 
-        mReportsHandler.setShouldDisconnectFromServiceChecker(new ShouldDisconnectFromServiceChecker() {
-            @Override
-            public boolean shouldDisconnect() {
-                return mainReporter.isPaused();
+        reportsHandler.setShouldDisconnectFromServiceChecker(
+            new ShouldDisconnectFromServiceChecker() {
+                @Override
+                public boolean shouldDisconnect() {
+                    return mainReporter.isPaused();
+                }
             }
-        });
+        );
 
-        mReporters.put(config.apiKey, mainReporter);
+        reporters.put(config.apiKey, mainReporter);
 
         return mainReporter;
     }
 
     @Override
     public synchronized void activateReporter(@NonNull ReporterConfig config) {
-        if (mReporters.containsKey(config.apiKey)) {
+        if (reporters.containsKey(config.apiKey)) {
             PublicLogger logger = LoggerStorage.getOrCreatePublicLogger(config.apiKey);
             if (logger.isEnabled()) {
                 logger.fw("Reporter with apiKey=%s already exists.", config.apiKey);
             }
         } else {
             getOrCreateReporter(config);
-            Log.i(SdkUtils.APPMETRICA_TAG, "Activate reporter with APIKey " + Utils.createPartialApiKey(config.apiKey));
+            Log.i(
+                SdkUtils.APPMETRICA_TAG,
+                "Activate reporter with APIKey " + Utils.createPartialApiKey(config.apiKey)
+            );
         }
     }
 
     @Override
     @NonNull
     public synchronized IReporterExtended getOrCreateReporter(@NonNull ReporterConfig config) {
-        IReporterExtended reporter = mReporters.get(config.apiKey);
+        IReporterExtended reporter = reporters.get(config.apiKey);
 
         if (reporter == null) {
-            if (mApiKeysToIgnoreStartup.contains(config.apiKey) == false) {
-                mStartupHelper.sendStartupIfNeeded();
+            if (!apiKeysToIgnoreStartup.contains(config.apiKey)) {
+                startupHelper.sendStartupIfNeeded();
             }
-            ManualReporter manualReporter = new ManualReporter(mContext,
-                    mProcessConfiguration,
-                    config,
-                    mReportsHandler);
+            ManualReporter manualReporter = new ManualReporter(
+                context,
+                processConfiguration,
+                config,
+                reportsHandler
+            );
             performCommonInitialization(manualReporter);
             manualReporter.start();
             reporter = manualReporter;
-            mReporters.put(config.apiKey, reporter);
+            reporters.put(config.apiKey, reporter);
         }
 
         return reporter;
@@ -120,16 +138,15 @@ public class ReporterFactory implements IReporterFactory {
 
     @Override
     @NonNull
-    public synchronized IUnhandledSituationReporter getMainOrCrashReporter(
-            @NonNull AppMetricaConfig config) {
-        IReporterExtended reporter = mReporters.get(config.apiKey);
+    public synchronized IUnhandledSituationReporter getMainOrCrashReporter(@NonNull AppMetricaConfig config) {
+        IReporterExtended reporter = reporters.get(config.apiKey);
         if (reporter == null) {
             DebugLogger.info(TAG, "No main reporter - will create crash reporter");
             CrashReporter crashReporter = new CrashReporter(
-                    mContext,
-                    mProcessConfiguration,
-                    config,
-                    mReportsHandler
+                context,
+                processConfiguration,
+                config,
+                reportsHandler
             );
             performCommonInitialization(crashReporter);
             crashReporter.updateConfig(config);
@@ -140,8 +157,8 @@ public class ReporterFactory implements IReporterFactory {
     }
 
     private void performCommonInitialization(@NonNull BaseReporter reporter) {
-        reporter.setKeepAliveHandler(new KeepAliveHandler(mTasksHandler, reporter));
-        reporter.setStartupParamsProvider(mStartupHelper);
+        reporter.setKeepAliveHandler(new KeepAliveHandler(tasksHandler, reporter));
+        reporter.setStartupParamsProvider(startupHelper);
     }
 
     @NonNull
