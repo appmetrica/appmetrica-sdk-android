b90c77c948c979632538f0ac5de98f24d0b24c60
robot-frankenstein <robot-frankenstein@yandex-team.com>
2024-06-26 18:16:41 +0300

    up AppMetrica SDK Android version to 7.0.0 f771394804904f65d75815c5eb212474a81bc2dd

--- analytics/src/main/java/io/appmetrica/analytics/impl/SdkData.java
+++ analytics/src/main/java/io/appmetrica/analytics/impl/SdkData.java
@@ -14,7 +14,7 @@ public final class SdkData {
     public static final int CURRENT = 114;
 
     //This version used for creation merge conflicts for parallel version or api level updating
-    public static final String CURRENT_VERSION_NAME_FOR_MAPPING = "6.6.0";
+    public static final String CURRENT_VERSION_NAME_FOR_MAPPING = "7.0.0";
 
     public static final int INITIAL_API_LEVEL = 112;
     public static final int MIGRATE_SESSION_SLEEP_START_TIME_TO_MILLISECONDS = 113;
