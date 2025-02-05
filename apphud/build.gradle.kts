516f164956afe21013350fed15ec19afcdd9c1a1
alexklints <alexklints@yandex-team.com>
2025-02-05 12:07:25 +0300

    update Apphud SDK to 2.8.4 commit_hash:993ff4cbc0bf3000343b9a991208f92372d32225

--- apphud/build.gradle.kts
+++ apphud/build.gradle.kts
@@ -29,5 +29,5 @@ dependencies {
     implementation(project(":modules-api"))
     implementation(project(":proto"))
 
-    implementation("com.apphud:ApphudSDK-Android:2.7.0")
+    implementation("com.apphud:ApphudSDK-Android:2.8.4")
 }
