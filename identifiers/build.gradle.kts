419707f38b58f5af8ba1dbe5c875e9cda11ac058
alexklints <alexklints@yandex-team.com>
2025-07-10 18:02:08 +0300

    revert com.google.android.gms:play-services-ads-identifier to version 18.1.0 commit_hash:b755317360a6de1eaba0cb070d48d252571942aa

--- identifiers/build.gradle.kts
+++ identifiers/build.gradle.kts
@@ -16,5 +16,5 @@ dependencies {
     implementation(project(":core-api"))
     implementation(project(":core-utils"))
     implementation(project(":logger"))
-    implementation("com.google.android.gms:play-services-ads-identifier:18.2.0")
+    implementation("com.google.android.gms:play-services-ads-identifier:18.1.0")
 }
