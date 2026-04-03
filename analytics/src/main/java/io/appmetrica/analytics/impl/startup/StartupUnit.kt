c9b48d8b0f3bbfd11ab77e802b442cb43fa4c6e6
alexklints <alexklints@yandex-team.com>
2026-04-04 02:22:28 +0300

    fix potential deadlock at io.appmetrica.analytics.impl.D4.a(SourceFile:18) commit_hash:7cadc001bcf20a106b4cf2d2777c949cfc5e4273

--- analytics/src/main/java/io/appmetrica/analytics/impl/startup/StartupUnit.kt
+++ analytics/src/main/java/io/appmetrica/analytics/impl/startup/StartupUnit.kt
@@ -25,6 +25,8 @@ internal class StartupUnit(
 
     private val tag = "[StartupUnit]"
 
+    private val currentTaskLock = Any()
+
     @Volatile
     private var currentTask: NetworkTask? = null
 
@@ -72,21 +74,21 @@ internal class StartupUnit(
         return startupUnitComponents.componentId
     }
 
-    @Synchronized
-    fun getOrCreateStartupTaskIfRequired(): NetworkTask? = if (isStartupRequired()) {
-        DebugLogger.info(tag, "getOrCreateStartupTaskIfRequired - startupRequired")
-        var task = currentTask
-        if (task == null || task.isRemoved) {
-            DebugLogger.info(tag, "getOrCreateStartupTaskIfRequired - create startup task")
-            task = createStartupTask(this, requestConfig)
-            currentTask = task
+    fun getOrCreateStartupTaskIfRequired(): NetworkTask? = synchronized(currentTaskLock) {
+        if (isStartupRequired()) {
+            DebugLogger.info(tag, "getOrCreateStartupTaskIfRequired - startupRequired")
+            var task = currentTask
+            if (task == null || task.isRemoved) {
+                DebugLogger.info(tag, "getOrCreateStartupTaskIfRequired - create startup task")
+                task = createStartupTask(this, requestConfig)
+                currentTask = task
+            }
+            task
+        } else {
+            null
         }
-        task
-    } else {
-        null
     }
 
-    @Synchronized
     fun isStartupRequired(
         identifiers: List<String>?,
         clidsFromClientForVerification: Map<String, String>
@@ -98,7 +100,6 @@ internal class StartupUnit(
         ) { startupUnitComponents.clidsStorage }
     }
 
-    @Synchronized
     fun isStartupRequired(): Boolean {
         if (GlobalServiceLocator.getInstance().dataSendingRestrictionController.isRestrictedForSdk) {
             DebugLogger.info(tag, "Startup restricted by data sending restriction policy.")
@@ -125,25 +126,18 @@ internal class StartupUnit(
         return required
     }
 
-    @Synchronized
-    private fun removeCurrentStartupTask() {
-        currentTask = null
-    }
-
     override fun onRequestComplete(
         result: StartupResult,
         requestConfig: StartupRequestConfig,
         responseHeaders: Map<String, List<String>>?
     ) {
-        var newState: StartupState
-        synchronized(this) {
-            val serverTime = StartupParser.parseServerTime(responseHeaders) ?: 0L
-            updateServerTime(result.validTimeDifference, serverTime)
-            newState = parseStartupResult(result, requestConfig, serverTime)
-            DebugLogger.info(tag, " new state $newState")
-            removeCurrentStartupTask()
-            updateCurrentStartupData(newState)
-        }
+        val newState: StartupState
+        val serverTime = StartupParser.parseServerTime(responseHeaders) ?: 0L
+        updateServerTime(result.validTimeDifference, serverTime)
+        newState = parseStartupResult(result, requestConfig, serverTime)
+        DebugLogger.info(tag, " new state $newState")
+        currentTask = null
+        updateCurrentStartupData(newState)
         notifyListener(newState)
     }
 
@@ -221,7 +215,6 @@ internal class StartupUnit(
         notifyListener(state)
     }
 
-    @Synchronized
     private fun updateCurrentStartupData(state: StartupState) {
         startupUnitComponents.startupConfigurationHolder.updateStartupState(state)
         startupUnitComponents.startupStateStorage.save(state)
@@ -239,11 +232,10 @@ internal class StartupUnit(
     }
 
     override fun onRequestError(cause: StartupError) {
-        removeCurrentStartupTask()
+        currentTask = null
         startupUnitComponents.resultListener.onStartupError(componentId.getPackage(), cause, startupState)
     }
 
-    @Synchronized
     fun updateConfiguration(arguments: StartupRequestConfig.Arguments) {
         DebugLogger.info(
             tag,
